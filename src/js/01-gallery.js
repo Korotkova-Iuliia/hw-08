96b4ef483e8bf80cd2d5397b3419f25c19c1d7ab
Korotkova-Iuliia <3589900@gmail.com>
2021-10-30 17:45:19 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+// console.log('Users/Юлия/GoIT/JS/JS_HomeWork_30-10/src/js/01-gallery.js: Not a directory');
