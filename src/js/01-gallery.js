fadd6a75f1c4d3774304cd2feaaa8739f8678cb4
Korotkova-Iuliia <3589900@gmail.com>
2021-10-30 18:35:10 +0300

    instal lightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
-// import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// console.log(galleryItems);
-// console.log('Users/Юлия/GoIT/JS/JS_HomeWork_30-10/src/js/01-gallery.js: Not a directory');
+console.log(galleryItems);
