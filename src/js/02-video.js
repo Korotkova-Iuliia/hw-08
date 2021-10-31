9f76549251b484996eadc6c9d853e8fd0c67c4b0
Korotkova-Iuliia <3589900@gmail.com>
2021-10-31 17:40:15 +0200

    import

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,16 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+console.log(iframe);
+console.log(player);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
 // const Player = require('@vimeo/player');
 
 // const iframe = document.querySelector('#vimeo-player');
