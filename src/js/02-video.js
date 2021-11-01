e7ebe39c46fbc35f8f3bb74154187b1c3d488353
Korotkova-Iuliia <3589900@gmail.com>
2021-11-02 00:00:54 +0200

    Revert "Revert "task 02""
    This reverts commit 07cf3107c054be113ccbd7796c62d1522ed59198.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,14 +9,11 @@ console.log(currentTimeStopPlayer);
 // const onPlaybackPause = function (data) {
 // const timeStopPlayer = data.seconds;
 // localStorage.setItem('videoplayer-current-time = data.seconds', timeStopPlayer);
-// console.log(timeStopPlayer);
-// console.log(currentTimeStopPlayer);
-// };
+// // };
 const onPlaybackPlay = function (data) {
   localStorage.setItem('videoplayer-current-time = data.seconds', data.seconds);
-  console.log(data);
-  console.log(data.seconds);
 };
+
 player
   .setCurrentTime(currentTimeStopPlayer)
   .then(function (seconds) {
@@ -37,9 +34,7 @@ player
 
 // const onPlaybackPlay = function (data) {
 //   localStorage.setItem('videoplayer-current-time = data.seconds', data.seconds);
-//   console.log(data);
-//   console.log(data.seconds);
-// };
-// const throttled = _.throttle(onPlaybackPlay, 1000);
+// // };
+// const throttled = throttle(onPlaybackPlay, 1000);
 
 // Player.on('timeupdate', throttled);
