2fb1d1bf61d79ca64c82f50bdf7b5592a5e3927d
Korotkova-Iuliia <3589900@gmail.com>
2021-11-01 12:10:05 +0200

    data.seconds

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,70 @@
 import Player from '@vimeo/player';
-var throttle = require('lodash.throttle');
+// var throttle = require('lodash.throttle');
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+console.log(player);
 // _.throttle(func, [(wait = 0)], [(options = {})]);
 // var throttled = _.throttle(renewToken, 300000, { trailing: false });
 // jQuery(element).on('click', throttled);
+// player.on('eventName', function (data) {
+//   // data is an object containing properties specific to that event
+// });
+// ?????????????????????????????????????????????????
+let currentTimeStopPlayer = localStorage.getItem('videoplayer-current-time = data.seconds');
+console.log(currentTimeStopPlayer);
 
+const callback = function (data) {
+  const timeStopPlayer = data.seconds;
+  localStorage.setItem('videoplayer-current-time = data.seconds', timeStopPlayer);
+  console.log(timeStopPlayer);
+  //   player.setCurrentTime(timeStopPlayer).then(function (timeupdate) {
+  //     console.log(timeupdate);
+  //   });
+};
+const onPlay = function (data) {
+  console.log(data);
+  timeOnPlayer = data.seconds;
+  console.log(timeOnPlayer);
+};
+
+const onPlayInput = function () {
+  //   iframe[this.name] = this.value;
+  console.log(iframe[data.seconds]);
+  console.log(data.seconds);
+};
+player.on('play', onPlay);
+player.on('input', onPlayInput);
+
+player.off('play', onPlay);
+player.on('pause', callback);
+
+player
+  .setCurrentTime(currentTimeStopPlayer)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+// ?????????????????????????????????????????????????
+
+// // // // // // // // // // // // // // // //
+// var throttle = require('lodash.throttle');
+
+// player.on('timeupdate', throttle(function (timeObj) {
+//     localStorage.setItem("videoplayer-current-time", JSON.stringify(timeObj.seconds));
+//     // console.log("timeUpdateFunc:", timeObj.seconds);
+// }, 1000));
+// // // // // // // // // // // // // // // // // // // // //
 // duration: 571.563;
 // percent: 0.113;
 // seconds: 64.533;
@@ -15,19 +74,15 @@ const player = new Player(iframe);
 // player.on('play', function () {
 //   console.log('played the video!');
 // });
-// const Player = require('@vimeo/player');
-
-// const iframe = document.querySelector('#vimeo-player');
-// console.log(iframe);
 
 // const player = new Vimeo.Player(iframe);
 // console.log(Vimeo.Player);
-// console.log(player);
+//
 // let timeCurrentPlayer = 'videoplayer-current-time';
 
 // player.on('timeupdate', function (data) {
 //   console.log(e.target);
-//   const timeStopPlayer = data.seconds;
+
 //   console.log(timeStopPlayer);
 // });
 // player
@@ -40,7 +95,6 @@ const player = new Player(iframe);
 //   });
 
 // const onPause = function (data) {
-//   console.log(this);
 //   const timeStopPlayer = data.seconds;
 //   console.log(timeStopPlayer);
 //   console.log(player.setCurrentTime);
@@ -56,23 +110,8 @@ const player = new Player(iframe);
 // };
 
 // const onPlayInput = function (e) {
-//   //   iframe[this.name] = this.value;
-//   // console.log(iframe[data.seconds]);
-//   console.log(e.target);
-//   // console.log(data.seconds);
+//   //   //   iframe[this.name] = this.value;
+//   //   // console.log(iframe[data.seconds]);
+//   //   console.log(e.target);
+//   console.log(data.seconds);
 // };
-// // player.on('play', onPlay);
-// player.on('input', onPlayInput);
-// // player.on('pause', onPause);
-// // player.off('play', onPlay);
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         data.seconds > data.duration;
-//         break;
-
-//       default:
-//         // data.seconds === 0;
-//         break;
-//     }
-//   });
